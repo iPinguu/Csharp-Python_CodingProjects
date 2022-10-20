2a55ff59bc6f5c56636d55057d4a1d63b70bc0a6
iPinguu <109537976+iPinguu@users.noreply.github.com>
2022-10-21 03:59:48 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# recursive import (*)
+from programs.EvenNumberPrograms import *
+
+# Even Number Programs:
+num = 10
+
+x = HowManyEvenNumbers(num)
+print(x)
+
+x1 = WhatAreTheNumbers(num)
+print(x1)
+
+x2 = WhatAreTheNumbersUsingArray(num)
+print(x2)
